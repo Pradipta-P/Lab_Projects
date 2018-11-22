a868dd02a19c5f2aebd51c4cac681b82a0ff6c30
Mantu <mantu@gmail.com>
2018-11-22 14:40:23 +0530

    final projects

---
+++ Demo/mod2/src/main/java/Lab_projects/App.java
@@ -0,0 +1,14 @@
+package Lab_projects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	System.out.println("King Yuvi is here");
+    }
+}
